4e561fcbbe8d111f0a1c38f855b8e4b996825196
shineslove <officialshineslove@gmail.com>
2023-08-26 15:32:53 -0400

    added extra tests

--- lexer/lexer_test.v
+++ lexer/lexer_test.v
@@ -4,15 +4,49 @@ import lexer
 import token as t
 
 fn test_next_token() {
-	input := '=+(){},;'
+	input := 'let five = 5;
+    let ten = 10;
+    let add = fn(x,y) {
+        x + y;
+    };
+    let result = add(five,ten);
+    '
 	tests := [
+        t.Token.let,
+        t.Token.ident,
 		t.Token.assign,
-		t.Token.plus,
+		t.Token.integer,
+		t.Token.semicolon,
+		t.Token.let,
+		t.Token.ident,
+		t.Token.assign,
+		t.Token.integer,
+		t.Token.semicolon,
+		t.Token.let,
+		t.Token.ident,
+		t.Token.assign,
+		t.Token.function,
 		t.Token.lparen,
+		t.Token.ident,
+		t.Token.comma,
+		t.Token.ident,
 		t.Token.rparen,
 		t.Token.lbrace,
+		t.Token.ident,
+		t.Token.plus,
+		t.Token.ident,
+		t.Token.semicolon,
 		t.Token.rbrace,
+		t.Token.semicolon,
+		t.Token.let,
+		t.Token.ident,
+		t.Token.assign,
+		t.Token.ident,
+		t.Token.lparen,
+		t.Token.ident,
 		t.Token.comma,
+		t.Token.ident,
+		t.Token.rparen,
 		t.Token.semicolon,
 		t.Token.eof,
 	]
